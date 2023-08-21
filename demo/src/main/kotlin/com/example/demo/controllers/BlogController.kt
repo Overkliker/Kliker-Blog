513a0f3ad496f04d50d8f96a7a8953fca79141d7
kliker <danilahreno@gmail.com>
2023-08-21 13:44:14 +0300

    add dynamic redirect for blog posts

--- demo/src/main/kotlin/com/example/demo/controllers/BlogController.kt
+++ demo/src/main/kotlin/com/example/demo/controllers/BlogController.kt
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.util.ArrayList
+import java.util.Optional
 
 @Controller
 class BlogController {
@@ -36,5 +39,20 @@ class BlogController {
         val post : Post =  Post(title = title, text = fullText)
         postRepository.save(post)
         return "redirect:/blog"
+    }
+
+    @GetMapping("/blog/{id}")
+    fun blogDetails(@PathVariable(value = "id") id : Int, model: Model): String{
+        if (postRepository.existsById(id)){
+            val post : Optional<Post> = postRepository.findById(id)
+            val toArr : ArrayList<Post> = ArrayList<Post>()
+            post.ifPresent(toArr::add)
+            model.addAttribute("post", toArr)
+            return "blog-details"
+        }
+        else{
+            return "redirect:/blog"
+        }
+
     }
 }
